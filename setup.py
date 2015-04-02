54620d7fc78c5c3b699427da7f67aa56838f7482
Tristan Penman <tristan@tristanpenman.com>
2015-04-02 23:08:13 +1100

    Add requirements.txt and update URL in setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='JSON Preprocessor',
     packages=['json_preprocessor'],
     scripts=['bin/json-preprocessor'],
+    url='https://bitbucket.org/tristanpenman/json-preprocessor',
     description='JSON Preprocessor library and command line utility that can '
                 'be used to resolve JSON References and other pre-processor '
                 'directives.',
