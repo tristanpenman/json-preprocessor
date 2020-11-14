f6abb367dbea3983c127afc55fbbd0e730193154
cvlvxi <cvlvxi@outlook.com>
2020-11-14 18:55:08 +1100

    remove reference to old bin/json-preprocessor

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from distutils.core import setup
 setup(
     name='JSON Preprocessor',
     packages=['json_preprocessor'],
-    scripts=['bin/json-preprocessor'],
     url='https://github.com/tristanpenman/json-preprocessor',
     description='JSON Preprocessor library and command line utility that can '
                 'be used to resolve JSON References and other pre-processor '
